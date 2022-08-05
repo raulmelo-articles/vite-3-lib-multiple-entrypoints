4c18f587611f16c20fad7221e22c0b98963bbeae
Raul Melo <melo.raulf@gmail.com>
2022-08-05 05:37:37 +0200

    chore: clean up demo app

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,10 +1,15 @@
 import "./style.css";
 import typescriptLogo from "./typescript.svg";
-import { setupCounter } from "./counter";
+
 import { sum } from "@examples/import-vite-build/math";
 import { logger } from "@examples/import-vite-build/logger";
 
+import { sum as sum2 } from "@examples/single-vite-config/math";
+import { logger as logger2 } from "@examples/single-vite-config/logger";
+
 logger.log(`Sum of 1 and 2 is ${sum(1, 2)}`);
+
+logger2.log(`Sum of 1 and 2 is ${sum2(1, 2)}`);
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
@@ -15,13 +20,8 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
 `;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
